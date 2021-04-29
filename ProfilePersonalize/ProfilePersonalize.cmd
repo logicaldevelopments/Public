f82c802b5f9a4b64288a88f1630ed98f0803f9d2
logicaldevelopments <itservices@logdev.com>
2021-04-28 19:34:41 -0700

    April/28/21 v3

--- ProfilePersonalize/ProfilePersonalize.cmd
+++ ProfilePersonalize/ProfilePersonalize.cmd
@@ -15,11 +15,15 @@ REM Set Measurement System (0 - Metric) (1 - U.S)
 reg add "HKEY_CURRENT_USER\Control Panel\International" /v "iMeasure" /d "1" /t REG_SZ /f
 
 REM Set Control Panel to show Small Icons
-reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel" /v "AllItemsIconView" /d "1" /t DWord /f
-reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel" /v "StartupPage" /d "1" /t DWord /f
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel" /v "AllItemsIconView" /d "1" /t REG_DWord /f
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel" /v "StartupPage" /d "1" /t REG_DWord /f
 
 REM Unhide Known File Extensions
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "HideFileExt" /d "0" /t DWord /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "HideFileExt" /d "0" /t REG_DWord /f
 
 REM Set Small Taskbar Icons
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "TaskbarSmallIcons" /d "0" /t DWord /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "TaskbarSmallIcons" /d "1" /t REG_DWord /f
+
+REM Hide Volume Icon from Notification Bar
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "HideSCAVolume" /d "1" /t REG_DWord /f
